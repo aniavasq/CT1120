13f76cbe0ba949a492844626aaf653471d386e97
aniavasq <aniavasq@gmail.com>
2020-11-14 10:19:54 -0500

    Update SelTest.java
    Último commit

--- src/main/java/seleniumEj/SelTest.java
+++ src/main/java/seleniumEj/SelTest.java
@@ -10,10 +10,11 @@ public class SelTest {
 
 	public static void main(String[] args) {
 		System.out.println("Test");
-		String exePath = Paths.get("").toAbsolutePath().toString() + File.separator + "drivers" + File.separator; // Setting path of the driver
-		System.setProperty("webdriver.chrome.driver", exePath + "chromedriver"); // Identifying the driver executable
-		WebDriver driver = new ChromeDriver(); // Execution time creation of Chrome driver
-		driver.get("https://www.cooltesters.com/tienda-de-cursos/selenium-con-java"); // Opening Chrome with specified URL
+		
+// 		String exePath = Paths.get("").toAbsolutePath().toString() + File.separator + "drivers" + File.separator; // Setting path of the driver
+// 		System.setProperty("webdriver.chrome.driver", exePath + "chromedriver"); // Identifying the driver executable
+// 		WebDriver driver = new ChromeDriver(); // Execution time creation of Chrome driver
+// 		driver.get("https://www.cooltesters.com/tienda-de-cursos/selenium-con-java"); // Opening Chrome with specified URL
 	}
 
 }
